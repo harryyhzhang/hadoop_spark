2962f8ab07673574334e7d45258a74999750e2c8
harry <harryyhzhang@gmail.com>
2018-04-05 13:38:58 +0000

    add the sparkr and all the images are referenced to harryyhzhang's docker hub

---
+++ startbuildimages.sh
@@ -0,0 +1,7 @@
+docker build -t harryyhzhang/base ./base
+docker build -t harryyhzhang/datanode ./datanode
+docker build -t harryyhzhang/namenode ./namenode
+docker build -t harryyhzhang/historyserver ./historyserver
+docker build -t harryyhzhang/nodemanager ./nodemanager
+docker build -t harryyhzhang/resourcemanager ./resourcemanager
+docker build -t harryyhzhang/sparkr ./sparkr
