357b70e4d3b7f9a87f588c2b863c83f2bb5fd2bd
harry <harryyhzhang@gmail.com>
2018-04-12 01:30:36 +0000

    change docker file and add docker-compose

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up 
