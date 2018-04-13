820f1ff046cee261db6c85272fe20d16bd9aba42
harry <harry>
2018-04-13 20:23:52 +0000

    change start with local docker-compose

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo cp ./bin/docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml up 
